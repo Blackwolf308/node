7a27453c02873e581ad39e533b2620a658d47ef6
BlackWolf308 <blackwolf3012@live.com>
2016-03-24 19:34:59 -0400

    some more notes for node

--- node_intro.js
+++ node_intro.js
@@ -2,6 +2,14 @@ var port    = 9000,
     express = require('express'),
     app     = express();
 
+var mysql = require('mysql')
+    danydb = mysql.createConection ({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'danydb'
+    });
+
 app.use('/test-man', express.static('test-man'));
 
 app.use(function(req, res, next) {
@@ -21,6 +29,45 @@ app.post("/test", function(req, res) {
 app.listen(port);
 console.log("node listening on port" + port);
 
-//   /test?fname=Daniel&lname=Alvarez
+/* /test?fname=Daniel&lname=Alvarez */
 /* http://localhost:9000/test-man/ */
 /* https://projecteuler.net/archives */
+/*-------------------------------------------------------------------------------------------*/
+// 1st part
+app.get('/dir/other/thing', function(req, res){
+    var str = "";
+    for (var x  in req.query) {
+        str += "<p>" + x + ", " + req.query[x] + "</p>"
+    }
+    res.writeHead(200, {"content-type": 'text/html'});
+    res.end(str);
+})
+
+// 2nd part
+app.post('/route/for/boobs', function(req, res){
+    var myQuery "INSERT INTO users (name, age) VALUES (\'" + req.query.name + "\', " + req.query.age + ")";
+    danydb.query(myQuery, function(err){
+        console.log(err);
+    })
+    var str = "Posted" + req.query.name + "and" + req.query.age  + "to danydb.users./n/n" + myQuery;
+    res.writeHead(200, {"content-type": 'text/html'});
+    res.end(str);
+});
+
+// 3rd part
+app.get('/ruta/lo/que/queremos', function(req, res){
+    var myQuery = "SELECT * FROM boob_chart";
+    danydb.query(myQuery, function(err, rows) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(rows);
+            res.end(JSON.stringify(rows));
+        }
+    });
+});
+
+/*  JSON: takes stuff from JS and turns them into text so that you can send them via petitions.
+    req: "request"
+    res: "response"
+    
--- test-man/js/test.js
+++ test-man/js/test.js
@@ -1,3 +1,5 @@
 $(document).on("click", function() {
     $(".row").toggleClass('hide');
 });
+
+// given an array containing only positive numbers, find the maximal product of two distinct cells.
